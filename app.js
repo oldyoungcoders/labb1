2088e7a1b1fe2ecc7712cd9de4864a9242279cf2
oldyoungcoders <132730577+oldyoungcoders@users.noreply.github.com>
2023-10-31 19:44:02 +0800

    Add files via upload
    lab1

---
+++ app.js
@@ -0,0 +1,91 @@
+// console.log("Hello World");
+// console.log("Bi" + 10);
+// console.log("Bi" + (10 + 20));
+// var a = 5;
+// var b = "text";
+// console.log( a + 10 -5 );
+// console.log( b + "bna" );
+
+// var c = "";
+// console.log( c );
+
+// var name = "Nandin";
+// console.log("Namaig " + name + " gedeg.");
+// var age = 10;
+// console.log("Bi " + age + " nastai.");
+// var isMale = false;
+// console.log( isMale);
+// var hooson, sav =null;
+// console.log( hooson );
+// console.log( sav );
+
+// console.log( c );
+
+// var name = "Nandin";
+// console.log("Minii neriig " + name + " gedeg.");
+// var age = 10;
+// console.log("Bi " + age + " nastai.");
+// var surdag = "Naran";
+// console.log("Bi " + surdag + " surguulid surdag.");
+// var isMale = false;
+// console.log("Bi eregtei gedeg n " + isMale);
+// var on = 2013;
+// console.log("Bi " + on + " ond tursun.");
+
+// console.log( c );
+
+// var t1 = 50, t2 = 100;
+// var niilber;
+// niilber = t1 + t2;
+// console.log(niilber);
+
+
+// console.log( c );
+
+var t3 = 18
+console.log(9/5*t3)+32  ;
+
+// console.log( c );
+
+var t4 = 64.4;
+console.log(5/9*(t4-32));
+
+// console.log( c );
+
+// var g1 = 3, g2 = 4, g3 = 5;
+// console.log("Gurvaljnii peremitr:" + (g1 + g2 + g3));
+
+// console.log( c );
+
+
+// var n1 = 12, n2 = 23;
+// console.log("Hariu:" + (n1 + n2));
+
+// console.log( c );
+
+// var k = 5;
+// console.log("Hariu:" + (k * k * k));
+// console.log("Hariu:" + (k * k * 6));
+
+// console.log( c );
+
+// var u1 = 4, u2 = 6;
+// console.log("Hariu:" + (u1 * u2));
+// console.log("Hariu:" + (2 * (u1 + u2)));
+
+// console.log( c );
+
+// // y=3x-5
+// var x = 5;
+// y = 3 * x - 5;
+// console.log("Hariu:" + (y));
+
+// console.log( c );
+
+// // y=4z2-3z+5
+// var z = 6;
+// l = (4 * z * 2) - (3 * z )+ 5;
+// console.log("Hariu:" + (l));
+
+
+
